68f20d7d03833edc5eb150af98411216f615bee7
SeitaRyuumon <ryuumon.seita@gmail.com>
2019-04-17 21:26:30 +0900

    modified:   main.rs "variable, scan, output"

--- main.rs
+++ main.rs
@@ -11,14 +11,18 @@ fn loop_input(mut __v: Vec<i64>) -> Vec<i64> {
 	return __v
 }
 fn main(){
-	let mut __num: i64;
+	let mut num: i64;
 	println!("start:");
-	__num = get_i64();
-	println!("num: {}",__num);
+	num = get_i64();
+	println!("num: {}", num);
 
 	let mut v: Vec<i64> = vec![];
-	v = loop_input(v);
-	for i in 0..__num {
-		println!("{} :{}",i as usize,v[i as usize]);
+	// v = loop_input(v);
+	for i in 0..num {
+		v.push(get_i64());
+	}
+	for i in 0..num {
+		// println!("{} :{}",i as usize,v[i as usize]);
+		println!("{} : {}",i,v[i as usize]);
 	}
 }
